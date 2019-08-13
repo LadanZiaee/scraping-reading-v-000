2b7daa6bc50ff02ca865e6321316867d26d38cca
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-13 15:57:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,9 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+course = doc.css(".title-oE5vT4")
 
-
-
+course.each do |course|
+  puts course.text.strip
+  end
